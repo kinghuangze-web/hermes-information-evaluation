06df53dd4c5859346b3cee5370a37963e4b5ae95
kinghuangze-web <kinghuangze@gmail.com>
2026-05-02 16:39:16 +0800

    Fix public repo data root

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,12 +1,21 @@
 const assert = require('node:assert/strict');
 
-const { getStorageSettings, DEFAULT_DATA_DIR, DATA_DIR_ENV_KEY } = require('../utils/storagePaths');
+const path = require('node:path');
+
+const {
+  getStorageSettings,
+  DEFAULT_DATA_DIR,
+  DATA_DIR_ENV_KEY,
+  PROJECT_ROOT,
+  getDataDir
+} = require('../utils/storagePaths');
 const { detectPlatform } = require('../utils/sourceLibrary');
 const { getHermesAgentExecutionConfig } = require('../hermes/multiAgentDispatcher');
 
 assert.equal(DEFAULT_DATA_DIR, 'data');
 assert.equal(DATA_DIR_ENV_KEY, 'HERMES_DATA_DIR');
 assert.equal(getStorageSettings().dataDir, 'data');
+assert.equal(getDataDir(), path.join(PROJECT_ROOT, 'data'));
 
 assert.equal(detectPlatform('https://x.com/example/status/123'), 'x');
 assert.equal(detectPlatform('https://mp.weixin.qq.com/s/example'), 'wechat');
